2f66eba018ec26e126ee59b6070bf8fd57463027
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2021-03-25 09:23:55 +0100

    Configurable cost model

--- src/optimize.jl
+++ src/optimize.jl
@@ -4,24 +4,28 @@ const DEFAULT_COMPILE_THRESHOLD = 1.04
 
 abstract type Optimizer end
 
-mutable struct TopNOptimizer <: Optimizer
+mutable struct TopNOptimizer{TCostModel} <: Optimizer
     n::Int
     compile_threshold::Float32
+    costmodel::TCostModel
     last_fixtypes
     fixtypes_history
-    TopNOptimizer(n = 10, compile_threshold = DEFAULT_COMPILE_THRESHOLD) = new(n, compile_threshold, EmptyTypeList, [])
+    TopNOptimizer(
+        n = 10;
+        compile_threshold = DEFAULT_COMPILE_THRESHOLD,
+        costmodel = basemodel) = new{typeof(costmodel)}(n, compile_threshold, costmodel, EmptyTypeList, [])
 end
 
 fixtypes(opt::TopNOptimizer, ::NoProfiler) = opt.last_fixtypes
 
 percent(x) = round((x) * 10_000) / 100
 
-function find_best_historic_cost(opt, _typefreqs)
+function find_best_historic_cost(opt, _typefreqs, costmodel)
     best_historic_cost = typemax(ClockCycle)
     best_idx = -1
     for idx in 1:length(opt.fixtypes_history) # TODO Do not calculate for all if the list is lengthy
         old = opt.fixtypes_history[idx]
-        oldcost = costof(_typefreqs, old)
+        oldcost = costof(_typefreqs, old, costmodel)
         if oldcost < best_historic_cost
             best_historic_cost = oldcost
             best_idx = idx
@@ -34,11 +38,11 @@ function fixtypes(opt::TopNOptimizer, prof::FullProfiler)
     tfs = typefreqs(prof)
     @debug "Profiled type freqs: $(tfs)"
     ideal = ideal_fixtypes(opt, prof)
-    idealcost = costof(tfs, ideal)
-    best_idx, best_historic_cost = find_best_historic_cost(opt, tfs)
+    idealcost = costof(tfs, ideal, opt.costmodel)
+    best_idx, best_historic_cost = find_best_historic_cost(opt, tfs, opt.costmodel)
     if best_historic_cost > opt.compile_threshold * idealcost
         @debug begin
-            lastcost = costof(typefreqs(prof), opt.last_fixtypes)
+            lastcost = costof(typefreqs(prof), opt.last_fixtypes, opt.costmodel)
             speedup_last = percent(1.0 - idealcost / lastcost)
             speedup_best = percent(1.0 - idealcost / best_historic_cost)
             "Selected new fixtype list for $(speedup_last)% speedup vs last used, and $(speedup_best)% vs best: $ideal"
